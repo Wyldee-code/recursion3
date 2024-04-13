9a2720b5499b8903087ee21d97b709b58898e2af
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-13 12:48:22 -0400

    solve

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -14,13 +14,24 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 
 function flatten(arr) {
   // Initialize an empty array to store the flattened elements
+  let result = []
   // iterate through each element of the input array
-  // recursively call flatten and add the results to the empty array
-  // else if not an arry push it into the empty array
+arr.forEach(element => {
+// check if the element is an array
+if (Array.isArray(element)){
+// recursively call flatten and add the results to the empty array
+result = result.concat(flatten(element))
+// else if not an arry push it into the empty array
+  }else {
+    result.push(element)
+  }
+});
   //return empty array
-
-
+  return result
 }
-
+debugger
+flatten([]); // []
+flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
